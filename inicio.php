f4ee3b32f041844a022e8a3403d1431be5cdb864
jonatan <jpizarroserrano@iesesteveterradas.cat>
2018-01-17 18:06:36 +0100

    .

--- inicio.php
+++ inicio.php
@@ -31,35 +31,35 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-	<title>Projecte Xat</title>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script type="text/javascript"></script>
-</head>
-<body>
-	<header>
-		<img src="imagenes/logo1.png">
-	</header>
-	<nav>
-		<div id='menu'>
-			<ul>
-				<li><a href=''>Ajuntament</a></li>
-				<li><a href=''>El municipi</a></li>
-				<li><a href=''>Serveis</a></li>
-				
-			</ul>
-		</div>
-		<div id='login'>
-			<form action="inicio.php" method="POST">Usuario: <input type="text" name="nombre"> Password: <input type="password" name="password"><input type="submit" value="Entrar"></form>
-		</div>
-	</nav>
-	<article>
-		<p></p>
-	</article>
-	<footer>
-		<p>Copiright 2018 - Projecte Xat</p>
-	</footer>
-</body>
+	<head>
+		<title>Projecte Xat</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		 <script src="js/script.js"></script> 
+	</head>
+	<body>
+		<header>
+			<img src="imagenes/logo.jpg">
+		</header>
+		<nav>
+			<div id='menu'>
+				<ul>
+					<li><a href=''>Ajuntament</a></li>
+					<li><a href=''>El municipi</a></li>
+					<li><a href=''>Serveis</a></li>
+				</ul>
+			</div>
+			<div id='login'>
+				<p><a>Registrarse</a> / <a onClick="entrar()">Entrar</a></p>
+				<!-- <form>Usuario: <input type="text" name="nombre"> Password: <input type="password" name="password"><input type="submit" value="Entrar"></form> -->
+			</div>
+		</nav>
+		<article>
+			<p></p>
+		</article>
+		<footer>
+			<p>Copiright 2018 - Projecte Xat</p>
+		</footer>
+	</body>
 
 </html>
