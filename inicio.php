18e8d3350ba2745c79d85363c9ba3c4b0f275906
jonatan <jpizarroserrano@iesesteveterradas.cat>
2018-01-17 17:51:28 +0100

    Registro , inicio .

--- inicio.php
+++ inicio.php
@@ -1,3 +1,36 @@
+<?php
+	session_start();
+	include 'connection.php';
+
+	if(isset($_SESSION["session_username"])){
+	// echo "Session is set"; 
+	header("Location: inicio.php");
+	}
+	 
+
+		
+	   echo "aa";
+		if(!empty($_POST['nombre']) && !empty($_POST['password']) ) {
+			$username=$_POST['nombre'];												//Asigna a las variables el post 
+			$password=$_POST['password']; 
+
+			 
+			
+			
+
+			if($username == "Anonimo" && $password == "Anonimo"){
+				echo "lol";	
+				$_SESSION['session_username']=$username;
+				header("Location: login.php");
+			 	
+
+		 	
+
+		}
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,10 +49,11 @@
 				<li><a href=''>Ajuntament</a></li>
 				<li><a href=''>El municipi</a></li>
 				<li><a href=''>Serveis</a></li>
+				
 			</ul>
 		</div>
 		<div id='login'>
-			<form>Usuario: <input type="text" name="nombre"> Password: <input type="password" name="password"><input type="submit" value="Entrar"></form>
+			<form action="inicio.php" method="POST">Usuario: <input type="text" name="nombre"> Password: <input type="password" name="password"><input type="submit" value="Entrar"></form>
 		</div>
 	</nav>
 	<article>
