714aca249c59e01a6e934637f70e65786b3c0950
jonatan <jpizarroserrano@iesesteveterradas.cat>
2018-01-17 17:58:29 +0100

    .

--- inicio.php
+++ inicio.php
@@ -7,9 +7,6 @@
 	header("Location: inicio.php");
 	}
 	 
-
-		
-	   echo "aa";
 		if(!empty($_POST['nombre']) && !empty($_POST['password']) ) {
 			$username=$_POST['nombre'];												//Asigna a las variables el post 
 			$password=$_POST['password']; 
@@ -33,7 +30,7 @@
 
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
+
 <head>
 	<title>Projecte Xat</title>
 	<meta charset="utf-8" />
@@ -64,36 +61,5 @@
 		<p>Copiright 2018 - Projecte Xat</p>
 	</footer>
 </body>
-=======
-	<head>
-		<title>Projecte Xat</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		 <script src="js/script.js"></script> 
-	</head>
-	<body>
-		<header>
-			<img src="imagenes/logo.jpg">
-		</header>
-		<nav>
-			<div id='menu'>
-				<ul>
-					<li><a href=''>Ajuntament</a></li>
-					<li><a href=''>El municipi</a></li>
-					<li><a href=''>Serveis</a></li>
-				</ul>
-			</div>
-			<div id='login'>
-				<p><a>Registrarse</a> / <a onClick="entrar()">Entrar</a></p>
-				<!-- <form>Usuario: <input type="text" name="nombre"> Password: <input type="password" name="password"><input type="submit" value="Entrar"></form> -->
-			</div>
-		</nav>
-		<article>
-			<p></p>
-		</article>
-		<footer>
-			<p>Copiright 2018 - Projecte Xat</p>
-		</footer>
-	</body>
->>>>>>> e0666207854160b50c20eca9a274897efcfd28cb
+
 </html>
